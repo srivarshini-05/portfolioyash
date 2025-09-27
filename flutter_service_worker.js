e529a8a3afdfed50af74a85b389ec55aa1fea65f
Yashwanth210613 <yashwanthb2106@gmail.com>
2025-09-27 20:05:32 +0530

    Deploy Flutter Web build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "16853c87abe1fff6c52a436f445ae49b
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "5083957b296b8f4e20c6c41282051463",
+"flutter_bootstrap.js": "94d5f14bbe3b1464b2cf899bcd1c9eb6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
